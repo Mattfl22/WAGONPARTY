dcc7dfd9786fd88f9ccda4e7741dc6c6a794cf2a
Vincent Orinel <vincent.orinel@gmail.com>
2022-02-21 16:46:00 +0100

    removing presence status true

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :wagon
 
-  validates :start_date, :end_date, :start_location, :end_location, :status, presence: true
+  validates :start_date, :end_date, :start_location, :end_location, presence: true
 end
