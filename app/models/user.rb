2f209ac47a0324ba21313d2e3356e3dd66272486
Floriane Hibelot <floriane@wemaintain.com>
2022-02-21 17:07:19 +0100

    add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # create an array with the value you show the user and the value you want to store
   USER_TYPE = [["I have a wagon to rent", "Provider"], ["I'm looking for a wagon to partayyy", "Client"]]
   validates :first_name, :last_name, :user_type, presence: true
   # check enum attribute
