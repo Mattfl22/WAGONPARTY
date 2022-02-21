6186b4d81a94c2889be888597fddf1670a5ad8b4
Matthieu Flecher <flecher.matthieu@gmail.com>
2022-02-21 14:09:58 +0100

    create wagon model with relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_130334) do
+ActiveRecord::Schema.define(version: 2022_02_21_130843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,15 @@ ActiveRecord::Schema.define(version: 2022_02_21_130334) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wagons", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "capacity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wagons_on_user_id"
+  end
+
+  add_foreign_key "wagons", "users"
 end
