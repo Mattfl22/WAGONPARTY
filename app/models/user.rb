503eb0e42ff52ab917e486d173d5cbdcb2b7572d
Floriane Hibelot <floriane@wemaintain.com>
2022-02-21 17:02:02 +0100

    add Provider and Client in the DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  USER_TYPE = ["I have a wagon to rent", "I'm looking for a wagon to partayyy"]
+  USER_TYPE = [["I have a wagon to rent", "Provider"], ["I'm looking for a wagon to partayyy", "Client"]]
   validates :first_name, :last_name, :user_type, presence: true
-  validates :user_type, inclusion: { in: USER_TYPE }
+  # check enum attribute
 end
