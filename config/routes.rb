0e9bf774375ab74bcfbb7bb0c9649d916016cbd4
Vincent Orinel <vincent.orinel@gmail.com>
2022-02-21 16:39:04 +0100

    bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :new, :create] do
-    resources :wagons, only: [:index, :show, :new, :create]
+    resources :wagons, only: [:index, :show, :new, :create] do
+      resources :bookings, only: [:new, :create]
+    end
   end
 end
