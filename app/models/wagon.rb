b52d401d5b68c889b4c41b115be930f0a67c7110
Vincent Orinel <vincent.orinel@gmail.com>
2022-02-23 11:30:58 +0100

    k

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,10 +1,7 @@
 class Wagon < ApplicationRecord
   belongs_to :user
   has_many :bookings
-<<<<<<< HEAD
   has_one_attached :photo
-=======
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_address?
->>>>>>> aedebf91dbed0727077a7cbea1fb638420f97695
 end
