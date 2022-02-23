44819477e481c7daff91b0d314cbfe38620b5e8d
Matthieu Flecher <flecher.matthieu@gmail.com>
2022-02-23 10:38:16 +0100

    add column to wagons

---
+++ db/migrate/20220223093243_change_available_dates_to_date.rb
@@ -0,0 +1,8 @@
+class ChangeAvailableDatesToDate < ActiveRecord::Migration[6.1]
+  def change
+    change_column :wagons, :available_dates, :string
+    add_column :wagons, :latitude, :float
+    add_column :wagons, :longitude, :float
+    add_column :wagons, :location, :string
+  end
+end
