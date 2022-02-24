69e155d7571338d1ef5ea20354e95f2a1997eb43
Floriane Hibelot <floriane@wemaintain.com>
2022-02-24 10:28:26 +0100

    commit changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_095654) do
+ActiveRecord::Schema.define(version: 2022_02_23_141950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
