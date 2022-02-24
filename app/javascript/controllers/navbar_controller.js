5c2934cb469444ef0d10cecaaf4b84f9d3a52c59
Martin Cavil <martincavil.dev@gmail.com>
2022-02-24 16:00:13 +0100

    Navbar dropdown fixed

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   updateNavbar() {
     if (window.scrollY >= window.innerHeight) {
       this.element.classList.add("navbar-black")
-      console.log(this.element)
     } else {
       this.element.classList.remove("navbar-black")
     }
