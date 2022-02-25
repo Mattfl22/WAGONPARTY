8cab581d83ef59fb940aae800cea1cfedf68de5d
Floriane Hibelot <floriane@wemaintain.com>
2022-02-25 11:50:16 +0100

    review system v1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -25,9 +25,26 @@ class BookingsController < ApplicationController
     redirect_to user_path(@booking.user)
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @user = current_user
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    authorize @booking
+    if @booking.save
+      redirect_to user_path(@booking.user)
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :start_location, :id)
+    params.require(:booking).permit(:start_date, :start_location, :id, :rating, :comment)
   end
 end
